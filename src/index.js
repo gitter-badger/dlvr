db5ec387cb156d63fe5bd3f11270faa7786520bc
FreaKzero <pete@freakzero.com>
2017-07-17 23:40:05 +0200

    Add Compress #1

--- src/index.js
+++ src/index.js
@@ -5,34 +5,61 @@ const spinner = require('./lib/spinner');
 const config = require('./lib/config');
 
 const git = require('./modules/git');
+const zip = require('./modules/zip');
 const runner = require('./modules/runner');
 const prompt = require('./modules/prompt');
 
 utils.bootMessage();
 
 config.loadConfig().then((config) => {
-  git.checkChanges().then(() => {
-    runner.runTests(config.test).then(() => {
-      prompt.version().then((version) => {
-        utils.saveVersion(version).then(() => {
-          git.commitAndPush(version).then(() => {
-            git.tagAndPush(version).then(() => {
-              runner.npmPublish(config.npmpublish).then(() => {
-                git.gitHubRelease(config, version).then((id) => {
-                  git.uploadAssets(config, id).then(() => {
-                    spinner.success();
-                    process.exit(0);
+  git.checkToken(config).then(() => {
+    git.checkChanges().then(() => {
+      runner.runTests(config.test).then(() => {
+        zip.compress(config).then(() => {
+          prompt.version().then((version) => {
+            utils.saveVersion(version).then(() => {
+              git.commitAndPush(version).then(() => {
+                git.tagAndPush(version).then(() => {
+                  runner.npmPublish(config.npmpublish).then(() => {
+                    git.gitHubRelease(config, version).then((id) => {
+                      git.uploadAssets(config, id).then(() => {
+                        spinner.success('Successful Released');
+                        process.exit(0);
+                      }).catch((err) => {
+                        spinner.fail(err.message);
+                      });
+                    }).catch((err) => {
+                      spinner.fail(err.message);
+                    });
+                  }).catch((err) => {
+                    spinner.fail(err.message);
                   });
+                }).catch((err) => {
+                  spinner.fail(err.message);
                 });
+              }).catch((err) => {
+                spinner.fail(err.message);
               });
+            }).catch((err) => {
+              spinner.fail(err.message);
             });
+          }).catch((err) => {
+            spinner.fail(err.message);
           });
         }).catch((err) => {
-          spinner.fail(err);
+          spinner.fail(err.message);
         });
       }).catch((err) => {
-        spinner.fail(err);
+        spinner.fail(err.message);
       });
+    }).catch((err) => {
+      spinner.fail(err.message);
+    }).catch((err) => {
+      spinner.fail(err.message);
     });
+  }).catch((err) => {
+    spinner.fail(err.message);
   });
+}).catch((err) => {
+  spinner.fail(err.message);
 });
--- src/modules/zip.js
+++ src/modules/zip.js
@@ -1,10 +1,36 @@
-zip = require('zip-local');
+const zip = require('zip-local');
+const asyncLoop = require('node-async-loop');
+const utils = require('../lib/utils');
+const spinner = require('../lib/spinner');
+const path = require('path');
 
-
-const zip = () => {
+const compress = (config) => {
   return new Promise((resolve, reject) => {
-  });
-}
-module.exports = {
+    if (config.compress) {
+      asyncLoop(config.compress, (item, next) => {
+        var fileIn = path.join(process.cwd(), item.in),
+          fileOut = path.join(process.cwd(), item.out);
 
-}
+        spinner.create(`Compressing ${item.in}`);
+        zip.zip(fileIn, (err, zipped) => {
+          utils.catchError(err, err, reject);
+          zipped.compress();
+
+          zipped.save(fileOut, (err) => {
+            utils.catchError(err, err, reject);
+            next();
+          });
+        });
+      }, (err) => {
+        utils.catchError(err, err, reject);
+        resolve();
+      });
+    } else {
+      resolve();
+    }
+  });
+};
+
+module.exports = {
+  compress
+};
